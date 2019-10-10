10b7305053d983e6f907bca7e6859c7c7d91f02f
matejslovak <joggurtik1@gmail.com>
2019-10-10 08:54:27 +0200

    Open-Lab-01.4

--- Open-Lab-01.4/Program.cs
+++ Open-Lab-01.4/Program.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_01._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type the base of the triangle and then enter its height");
+            string siteA = Console.ReadLine();
+            string height = Console.ReadLine();
+            int a = Convert.ToInt32(siteA);
+            int b = Convert.ToInt32(height);
+            int c = a * b / 2;
+            Console.WriteLine($"The area of ​​the triangle is {c}.");
         }
     }
 }
